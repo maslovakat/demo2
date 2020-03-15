d0d4045521acf8da0fcc5096a5048dcacb46ce95
Giruta <irina.giruta@gmail.com>
2020-03-16 00:59:36 +0200

    change ProductListView and index.html

--- app/components/productList/ProductListView.js
+++ app/components/productList/ProductListView.js
@@ -5,32 +5,35 @@ export class ProductListView {
 
     renderList(arr) {
         this.container.innerHTML="";
-        arr.forEach((el, ind, arr) => {
+        arr.forEach(el => {
             let productCard = this.createProductCard(el);
             this.container.appendChild(productCard);
         });
-
     }
-
+    
     createProductCard (prod) { // будет создавать карточки
         const card = document.createElement('div');
-
+        
         card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
-        card.innerHTML = `<div class="card">
-                            <div class="card-img-wrapper">
-                                <img class="card-img-top" src="${prod.image}" alt="">
-                                <button class="card-add-wishlist"
-                                        data-goods-id="${prod.id}"></button>
+        
+        card.innerHTML = `<div class="card border-secondary h-100">  
+                            <div class = "card-img-wrapper">    
+                                <img src="${prod.image}" class="card-img-top" alt="...">
                             </div>
-                            <div class="card-body justify-content-between">
-                                <a href="#" class="card-title">${prod.breed}</a>
-                                <div class="card-price">${prod.price} ₽</div>
-                                <div>
-                                    <button class="card-add-cart"
-                                            data-goods-id="${prod.id}">Добавить в корзину</button>
-                                </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <p class="card-title">${prod.breed}</p>
+                                    <p class="card-text">${prod.species}  ${prod.gender}</p>
+                                    <p class="card-age"> Age: ${prod.birth_date}</p>
+                                </li>
+                                <li class="list-group-item card-price">Price: $ ${prod.price}</li>
+                            </ul>
+                            <div class="card-body">
+                                <a href="#" class="card-link card-link_green">Add to Cart</a>
+                                <a href="#" class="card-link card-link_blue">Details</a>
                             </div>
-                        </div>`
+                         </div>   
+                        `
         return card;
     };
 }
